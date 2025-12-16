3c853599b72a983ef489449da45114803aa7c03a
AndresPerez <47432061+andresperez019@users.noreply.github.com>
2025-12-16 16:34:08 -0700

    modify CityDetail.vue

--- src/views/CityDetail.vue
+++ src/views/CityDetail.vue
@@ -1,15 +1,22 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { getForecast } from '../api/weather'
 
 const route = useRoute()
 const forecast = ref([])
 
-onMounted(async () => {
-  const { data } = await getForecast(route.params.lat, route.params.lon)
-  forecast.value = data.result.daily
-})
+const fetchForecast = async () => {
+  const { lat, lon } = route.params
+  const { data } = await getForecast(lat, lon)
+  forecast.value = data.response.result.list
+}
+
+watch(
+  () => route.params,
+  fetchForecast,
+  { immediate: true }
+)
 </script>
 
 <template>
@@ -18,8 +25,8 @@ onMounted(async () => {
 
     <div v-for="day in forecast" :key="day.dt">
       <p>{{ new Date(day.dt * 1000).toLocaleDateString() }}</p>
-      <p>Max: {{ day.temp.max }}°C</p>
-      <p>Min: {{ day.temp.min }}°C</p>
+      <p>Max: {{ day.main.temp_max }}°C</p>
+      <p>Min: {{ day.main.temp_min }}°C</p>
       <p>{{ day.weather[0].description }}</p>
     </div>
   </div>
